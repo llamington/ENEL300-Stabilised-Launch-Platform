3fad132fe432229c21678e3aab1dbeff0da08669
Bailey <lissington4@gmail.com>
2022-08-31 19:41:14 +1200

    created an accelerometer module

--- software/Core/Inc/accelerometer.h
+++ software/Core/Inc/accelerometer.h
@@ -1,4 +1,20 @@
 #ifndef __ACCELEROMETER_H
 #define __ACCELEROMETER_H
 
+typedef struct
+{
+    int32_t x, y, z;
+} acceleration_t;
+
+/**
+ * @brief Begin read of accelerometer's acceleration_data
+ */
+void begin_accelerometer_read(void);
+
+/**
+ * @brief Read the most recent acceleration data.
+ * @param accel: Acceleration data structure
+ */
+void get_acceleration(acceleration_t *accel);
+
 #endif
